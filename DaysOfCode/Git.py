480ca925f8b67b7663b368e00f20a8c4ce99e70e
Peter McDonald <pete@petermcdonald.co.uk>
2019-01-11 17:12:07 +0000

    Windows create file issue (#3)
    * correcting write issue on windows
    
    * rewrite of DaysOfCode.py with git additions specifying the path

--- DaysOfCode/Git.py
+++ DaysOfCode/Git.py
@@ -7,7 +7,7 @@ class GitException(Exception):
 
 
 class Git:
-    _path: str = ''
+    __slots__ = ['_path']
 
     def __init__(self, path: str) -> None:
         if not os.path.exists(path):
@@ -15,25 +15,34 @@ class Git:
         self._path = path
 
     def clone_remote(self, url: str) -> None:
-        os.chdir(self._path)
-        subprocess.call(['git', 'clone', url], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+        subprocess.call(['git', '-C', self._path, 'clone', url],
+                        shell=False,
+                        stderr=subprocess.DEVNULL,
+                        stdout=subprocess.DEVNULL)
 
     def push(self, repository: str) -> None:
-        if not os.path.exists(os.path.join(self._path, repository)):
-            raise GitException('Given path is not available')
-        os.chdir(os.path.join(self._path, repository))
-        subprocess.call(['git', 'push', ], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+        path = os.path.join(self._path, repository)
+        if not os.path.exists(path):
+            raise GitException('Given repository is not available')
+        subprocess.call(['git', '-C', self._path, 'push', ],
+                        shell=False,
+                        stdout=subprocess.DEVNULL,
+                        stderr=subprocess.DEVNULL)
 
     def add_file(self, repository: str) -> None:
-        os.chdir(os.path.join(self._path, repository))
-        subprocess.call(['git', 'add'],
+        path = os.path.join(self._path, repository)
+        if not os.path.exists(path):
+            raise GitException('Given repository is not available')
+        subprocess.call(['git', '-C', self._path, 'add', ],
                         shell=False,
                         stdout=subprocess.DEVNULL,
                         stderr=subprocess.DEVNULL)
 
     def commit(self, repository: str, message: str = '') -> None:
-        os.chdir(os.path.join(self._path, repository))
-        subprocess.call(['git', 'commit', '-m', message],
+        path = os.path.join(self._path, repository)
+        if not os.path.exists(path):
+            raise GitException('Given repository is not available')
+        subprocess.call(['git', '-C', self._path, 'commit', '-m', message, ],
                         shell=False,
                         stdout=subprocess.DEVNULL,
                         stderr=subprocess.DEVNULL)
